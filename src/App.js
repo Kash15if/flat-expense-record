ecdc66a9737e89e798822a497f1c641ebd61b638
Kash15if <kash15if@yahoo.com>
2021-12-05 16:26:47 +0530

    added dark mode

--- src/App.js
+++ src/App.js
@@ -1,85 +1,116 @@
 import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
-
+import { ThemeProvider } from "@mui/material/styles";
 import { getAllData } from "./Redux/actions";
-//import { fetchUserById } from "./Redux/actions";
-
 import React, { useEffect } from "react";
 import Home from "./Pages/Home";
 import Member from "./Pages/Members";
 import AdminPage from "./Admin/ManageExpense";
-
+import { darkTheme } from "./Theme.js";
+import CssBaseline from "@mui/material/CssBaseline";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Slide from "@mui/material/Slide";
+
+function HideOnScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func,
+};
 
 function App() {
   const dispatch = useDispatch();
-
   const data = useSelector((state) => state.paid);
 
   useEffect(() => {
     dispatch(getAllData());
   }, [dispatch]);
 
-  //console.log(data.value);
-  //console.log(data.lineData);
-  // console.log(data.pieData);
-
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              data.value !== "Loading" && (
-                <Home
-                  pieChartData={data.pieData}
-                  lineChartData={data.lineData}
-                  tableData={data.value}
-                />
-                /* <AdminPage /> */
-              )
-            }
-          />
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <HideOnScroll>
+          <AppBar>
+            <Toolbar>
+              <Typography variant="h6" component="div">
+                Flat Expense Record
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </HideOnScroll>
+        <Toolbar />
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                data.value !== "Loading" && (
+                  <Home
+                    pieChartData={data.pieData}
+                    lineChartData={data.lineData}
+                    tableData={data.value}
+                  />
+                  /* <AdminPage /> */
+                )
+              }
+            />
 
-          <Route
-            path="/admin/add/login"
-            element={data.value !== "Loading" && <AdminPage />}
-          />
+            <Route
+              path="/admin/add/login"
+              element={data.value !== "Loading" && <AdminPage />}
+            />
 
-          <Route
-            path="/kashif"
-            element={
-              data.value !== "Loading" && (
-                <Member
-                  name="kashif"
-                  pieChartData={data.pieData}
-                  lineChartData={data.lineData}
-                  tableData={data.value}
-                />
-              )
-            }
-          />
+            <Route
+              path="/kashif"
+              element={
+                data.value !== "Loading" && (
+                  <Member
+                    name="kashif"
+                    pieChartData={data.pieData}
+                    lineChartData={data.lineData}
+                    tableData={data.value}
+                  />
+                )
+              }
+            />
 
-          <Route
-            path="/azarul"
-            element={
-              data.value !== "Loading" && (
-                <Member
-                  name="azarul"
-                  pieChartData={data.pieData}
-                  lineChartData={data.lineData}
-                  tableData={data.value}
-                />
-                /* <AdminPage /> */
-              )
-            }
-          />
-        </Routes>
-      </Router>
+            <Route
+              path="/azarul"
+              element={
+                data.value !== "Loading" && (
+                  <Member
+                    name="azarul"
+                    pieChartData={data.pieData}
+                    lineChartData={data.lineData}
+                    tableData={data.value}
+                  />
+                  /* <AdminPage /> */
+                )
+              }
+            />
+          </Routes>
+        </Router>
 
-      {/* <div>{data.id}</div> */}
-      {/* <AdminPage /> */}
+        {/* <div>{data.id}</div> */}
+        {/* <AdminPage /> */}
+      </ThemeProvider>
     </div>
   );
 }
